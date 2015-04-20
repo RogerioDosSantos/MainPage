2f7ef00ec409b17955be92ebe55982c892b0d5fb
Rogerio dos Santos <rogerio.santos.main@gmail.com>
2015-04-19 22:15:01 -0500

    Implemented Search

---
+++ js/search.js
@@ -0,0 +1,91 @@
+﻿(function (ns, Url) {
+
+    var _webSearch = new google.search.WebSearch();
+    var _options;
+
+    function getUrlOptions() {
+        var parameters = new Map();
+        var result = {
+            parameters: parameters,
+            site: ""
+        };
+
+        if (Url == null)
+            return result;
+
+        var values = document.URL.split('?');        
+        var site = values[0];
+        site = site.substring(0, site.length - 12)
+        result.site = site;
+        values.splice(0, 1);
+        if (values.length == 0)
+            return result;
+
+        var values = values[0].split('&');
+        for (var index = 0, pair; index < values.length; index++) {
+            keyValue = values[index].split('=');
+            if (keyValue.length < 2)
+                continue;
+
+            var key = unescape(keyValue[0]);
+            var value = decodeURIComponent(keyValue[1]);
+            parameters[key] = value;
+        }
+        
+        result.parameters = parameters;
+        return result;
+    }
+
+    function buildSearchLineElement(url, title, content) {        
+        var ret = "<tr class='search result row'>" +
+            "<td>" + title + "<br/>" + content + "</td>" +
+            "<td>" + url + "</td>" +
+            "</tr>";
+        return ret;
+    }
+
+    function webSearchComplete(searcher, searchNum) {
+        var results = searcher.results;
+        var lines = ""
+        var search = searcher.hf;
+        results.forEach(function (result, index, results) {
+            lines += buildSearchLineElement(result.unescapedUrl, result.titleNoFormatting, result.content);
+        });
+
+        var query = _options.parameters["search"];
+        if (lines == "")
+            query = "<div>Any Result was found</div><br />" + query;
+
+        var queryHtml = "<div class='search query'>" + query + "</div>"
+        $("div.search.query").replaceWith(queryHtml);
+        if (lines == "")
+            return;
+
+        $("div.search.query").replaceWith(queryHtml);
+        $("tr.search.result.row").remove();
+        $("table.search.result").append(lines);
+    }
+
+ 
+    function performSearch() {        
+        _options = getUrlOptions();
+        var search = _options.parameters["search"];
+        if (search != null) {
+            search = "site:" + _options.site + " " + search;
+            _webSearch.execute(search);
+        }
+    }
+
+    function actions() {
+        performSearch();
+    }
+
+    _webSearch.setSearchCompleteCallback(this, webSearchComplete, [_webSearch]);
+
+    //Assure the listeners to be executed after the page is loaded.
+    var g_dependencies = $.when($.Deferred(function (deferred) { $(deferred.resolve); }));
+    g_dependencies.fail(function () { console.log("Fail to load dependencies"); });
+    g_dependencies.done(actions);
+
+})(this,
+document.URL)
